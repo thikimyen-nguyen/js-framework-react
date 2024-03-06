ecfc7cd17cb6e76d0760bc4222a2abdce8d9bb03
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-03-06 22:16:33 +0100

    add form validation

--- babee-shop/src/components/ContactForm/index.jsx
+++ babee-shop/src/components/ContactForm/index.jsx
@@ -1,73 +1,93 @@
-import { useState } from 'react';
-
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+const schema = yup
+    .object({
+        fullName: yup
+        .string()
+        .min(3, 'Your full name should be at least 3 characters.')
+        .required('Please enter your first name.'),
+        subject: yup
+        .string()
+        .min(3, 'Subject should be at least 3 characters.')
+        .required('Please enter subject'),
+        email: yup
+        .string()
+        .email('Please enter a valid email address.')
+        .required('Please enter your email.'),
+        body: yup
+        .string()
+        .min(3, 'Your message should be at least 3 characters.')
+        .required('Please enter your message.'),
+    })
+    .required();
 export function ContactForm() {
-    // const [firstName, setFirstName] = useState('');
-    // const [lastName, setLastName] = useState('');
-    // const [city, setCity] = useState('');
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        resolver: yupResolver(schema),
+      });
+    
+    function onSubmit(data) {
+        console.log(data);
+    }
 
-    // function onTextInputChange(event) {
-    //     const value = event.target.value;
-    //     if (event.target.name === 'first-name') {
-    //       setFirstName(value);
-    //     }
-    //     if (event.target.name === 'last-name') {
-    //       setLastName(value);
-    //     }
-    //     if (event.target.name === 'city') {
-    //       setCity(value);
-    //     }
-    //   }
     return (
-        <div >
-            <h1 className="text-center">Contact Form</h1>
-            <form className="flex-col content-center md:w-2/4 mx-auto bg-black p-7 border-black rounded mt-4 ">
+        <div className="min-h-screen ">
+            <h1 className='text-center'>Contact Form</h1>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex-col md:w-2/4 content-center mx-auto">
                 <div className="mb-4">
-                    <label htmlFor="fullName" className="block font-semibold">Full Name</label>
+                    <label htmlFor="fullName" className="font-semibold">Full Name</label>
                     <input
                         type="text"
                         id="fullName"
-                        name="fullName"
-                        className="mt-1 p-2 border rounded w-full text-black"
-                        required
-                        minLength="3"
+                        {...register('fullName')}
+                        className={`mt-1 p-2 text-black ${errors.fullName ? 'error-border' : 'border-gray-300'} rounded w-full`}
+                        placeholder='John Smith'
                     />
+                    <p className='text-primary'>{errors.fullName?.message}</p>
                 </div>
                 <div className="mb-4">
                     <label htmlFor="subject" className="block text-gray-600 font-semibold">Subject</label>
                     <input
                         type="text"
                         id="subject"
-                        name="subject"
-                        className="mt-1 p-2 border rounded w-full text-black"
-                        required
-                        minLength="3"
+                        {...register('subject')}
+                        className={`mt-1 p-2 text-black ${errors.subject ? 'error-border' : 'border-gray-300'} rounded w-full`}
+                        placeholder='Your subject'
                     />
+                    <p className='text-primary'>{errors.subject?.message}</p>
                 </div>
                 <div className="mb-4">
-                    <label htmlFor="email" className="block text-gray-600 font-semibold">Email</label>
+                    <label htmlFor="email" className="block font-semibold">Email</label>
                     <input
                         type="email"
                         id="email"
-                        name="email"
-                        className="mt-1 p-2 border rounded w-full text-black"
-                        required
-                        pattern="^\S+@\S+$"
+                        {...register('email')}
+                        className={`mt-1 p-2 text-black ${errors.email ? 'error-border' : 'border-gray-300'} rounded w-full`}
+                        placeholder='abc@mail.com'
                     />
+                    <p className='text-primary'>{errors.email?.message}</p>
+
                 </div>
                 <div className="mb-4">
-                    <label htmlFor="body" className="block text-gray-600 font-semibold">Body</label>
+                    <label htmlFor="body" className="block font-semibold">Body</label>
                     <textarea
                         id="body"
-                        name="body"
-                        className="mt-1 p-2 border border-gray-300 rounded w-full text-black"
-                        required
-                        minLength="3"
+                        {...register('body')}
+                        className={`mt-1 p-2 text-black ${errors.body ? 'error-border' : 'border-gray-300'} rounded w-full`}
+                        placeholder='Your message goes here...'
                     ></textarea>
+                    <p className='text-primary'>{errors.body?.message}</p>
+
                 </div>
-                <div className="mt-4">
+                <div className="mt-4 text-center">
                 <button type="submit" className="primary-button">Submit</button>
                 </div>
             </form>
         </div>
     );
+   
 }
