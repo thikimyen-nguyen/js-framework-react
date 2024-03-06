cc2e872e3fe14be2fbc70290c14e28207a1c8395
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-03-06 11:06:23 +0100

    create header

---
+++ babee-shop/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary': '#FFCA3A',
+      'black': '#14130F',
+    },
+  },
+  plugins: [],
+}
+
