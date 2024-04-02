a8b5d193045facd1ce8138c88296dae6a9d32a6f
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-04-02 14:01:20 +0200

    fix product card

--- src/components/Products/productCard.jsx
+++ src/components/Products/productCard.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import { PrimaryButton } from '../Buttons'
-import { Link } from 'react-router-dom'
-import Discount from '../DiscountNote'
+import React from "react";
+import { PrimaryButton } from "../Buttons";
+import { Link } from "react-router-dom";
+import Discount from "../DiscountNote";
 
 function ProductCard({
-    product: { id, title, discountedPrice, image, price }
-  }) {
+  product: { id, title, discountedPrice, image, price },
+}) {
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4 relative">
-        <div id={id} className="p-3 group flex flex-col rounded-lg overflow-hidden bg-black hover:shadow-md transition duration-300">
-            <img
-                src={image.url}
-                alt={title}
-                className="w-full h-48 object-cover object-center group-hover:opacity-75"
-            />
-            <h2 className="mt-4 text-primary">{title}</h2>
-            <p className="m-2 text-lg font-medium text-right">Kr{discountedPrice}</p>
-            <Link key={id} to={`/${id}`} className='text-center'><PrimaryButton label='View Product' /></Link>
+      <div
+        id={id}
+        className="p-3 group flex flex-col rounded-lg overflow-hidden bg-black hover:shadow-md transition duration-300"
+      >
+        <img
+          src={image.url}
+          alt={title}
+          className="w-full h-48 object-cover object-center group-hover:opacity-75"
+        />
+        <h2 className="mt-4 text-primary">{title}</h2>
+        <div className="my-3 flex items-center">
+          <p className="m-2 text-lg font-medium">
+            Nok {discountedPrice}
+          </p>
+          {discountedPrice !== price && (
+            <p className="ml-5 line-through text-red text-sm">{price}</p>
+          )}
         </div>
-        {price !== discountedPrice && (
-          <div className='absolute top-0 right-0'>
-          <Discount price={price} discountedPrice={discountedPrice}/>
-          </div>
-        )}
-       
+
+        <Link key={id} to={`/${id}`} className="text-center">
+          <PrimaryButton label="View Product" />
+        </Link>
+      </div>
+      {price !== discountedPrice && (
+        <div className="absolute top-0 right-0">
+          <Discount price={price} discountedPrice={discountedPrice} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default ProductCard
-
+export default ProductCard;
