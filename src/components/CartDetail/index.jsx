bdf0309e26ced99cfdc28f9b963847395bb07741
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-04-03 12:49:12 +0200

    add cart total

--- src/components/CartDetail/index.jsx
+++ src/components/CartDetail/index.jsx
@@ -1,27 +1,55 @@
 import { Link } from "react-router-dom";
 import { PrimaryButton, SecondaryButton } from "../Buttons";
+import useProductsStore from "../../store/products";
 
-function CartItem ({
-    product: { id, title, discountedPrice, image, price },
-  }) {
-    return <div>heloo</div>
+function CartItem({
+  product: { id, title, discountedPrice, image, price, quantity },
+}) {
+  return (
+    <div className="flex m-3 ">
+      <div className="h-24 w-24 object-cover object-center p-2">
+        <img
+          src={image.url}
+          alt={title}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+
+      <div className="flex grow justify-between p-2">
+        <p className="flex-1">{title}</p>
+        <div className="flex flex-1">
+          <p className="flex-1">x {quantity}</p>
+          <p className="flex-1 text-right">Nok {discountedPrice}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export function CartDetail() {
-    const cart = JSON.parse(localStorage.getItem('cart'))
-    
-    return (
-    <div className="flex-col">
-        <h1>Your Cart</h1>
-       {cart?.map((product) => (
-            <CartItem product={product} />
-          ))
-       } 
-        <div className="text-center">
-        <Link to='/'><SecondaryButton label="Shop More"/></Link>
-        <Link to='/checkoutSuccess'><PrimaryButton label="Check Out" /></Link>
+  const cart = JSON.parse(localStorage.getItem("cart"));
+const {getTotalPrice} = useProductsStore();
+  return (
+    <div className="w-full md:w-3/4 xl:w-1/2 m-auto">
+      <h1 className="text-center">Your Cart</h1>
+      <div className="flex-col divide-y divide-primary mb-5">
+        {cart?.map((product) => (
+          <CartItem product={product} />
+        ))}
+        <div className="flex items-center text-xl text-primary text-end my-5 ">
+            <p className="text-xl flex-1 my-5">Total:</p>
+            <p className="flex-1 my-5">Nok {getTotalPrice()}</p>
         </div>
-        
+      </div>
+
+      <div className="text-center">
+        <Link to="/">
+          <SecondaryButton label="Shop More" />
+        </Link>
+        <Link to="/checkoutSuccess">
+          <PrimaryButton label="Check Out" />
+        </Link>
+      </div>
     </div>
-    );
+  );
 }
--- src/store/products.jsx
+++ src/store/products.jsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { allProductsUrl } from "../api";
 const getInitialCartState = () => {
-  const storedCart = localStorage.getItem('cart');
+  const storedCart = localStorage.getItem("cart");
   return storedCart ? JSON.parse(storedCart) : [];
 };
 
@@ -31,9 +31,8 @@ const useProductsStore = create((set, get) => ({
       const response = await fetch(singleProductUrl);
       const json = await response.json();
       set((state) => ({ ...state, singleProduct: json.data }));
-      set((state) => ({ ...state, productReviews: json.data.reviews}));
-      localStorage.setItem('currentProduct', JSON.stringify(json.data));
-
+      set((state) => ({ ...state, productReviews: json.data.reviews }));
+      localStorage.setItem("currentProduct", JSON.stringify(json.data));
     } catch (error) {
       set({ isError: true });
     } finally {
@@ -43,38 +42,48 @@ const useProductsStore = create((set, get) => ({
   addToCart: (product) => {
     set((state) => {
       const existingProductIndex = state.cart.findIndex(
-        (currentProduct) => product.id === currentProduct.id,
+        (currentProduct) => product.id === currentProduct.id
       );
-  
+
       let updatedCart;
-  
+
       if (existingProductIndex !== -1) {
         updatedCart = [...state.cart];
         // Increment quantity of the existing product in the cart
         updatedCart[existingProductIndex].quantity += 1;
       } else {
         // Add a new product to the cart with quantity 1
-        updatedCart = [...state.cart, { id: product.id, title: product.title, quantity: 1 }];
+        updatedCart = [
+          ...state.cart,
+          {
+            id: product.id,
+            title: product.title,
+            quantity: 1,
+            image: product.image,
+            discountedPrice: product.discountedPrice,
+          },
+        ];
       }
-  
+
       // Update local storage with the updated cart
-      localStorage.setItem('cart', JSON.stringify(updatedCart));
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
       console.log(updatedCart);
-      
+
       return { ...state, cart: updatedCart };
     });
-  
   },
 
-  getTotalNumberOfItemsInCart: () => { 
-    const cart = get().cart; 
+  getTotalNumberOfItemsInCart: () => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
     return cart ? cart.length : 0;
   },
-  
-  
-  
-  
-  
+  getTotalPrice: () => {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    if (!cart || cart.length === 0) {
+      return 0;
+    }
+    return cart.reduce((total, item) => total + (item.discountedPrice * item.quantity), 0).toFixed(2);
+  },
 }));
 
 export default useProductsStore;
